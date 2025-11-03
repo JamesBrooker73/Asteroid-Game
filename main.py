93447a9735d762db5d5e647c209bb7b6d261373c
James Brooker <james.m.brooker8@gmail.com>
2025-11-03 17:51:22 +0000

    Added asteroid.py, asteroidfield.py and player.py. Added more logic into main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -12,8 +15,20 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     clock = pygame.time.Clock()
     dt = 0
+
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidField = AsteroidField()
+    
 
     while True:
         for event in pygame.event.get():
@@ -21,8 +36,15 @@ def main():
                 pygame.quit()
                 return
         screen.fill("black")
+
+        updateable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
